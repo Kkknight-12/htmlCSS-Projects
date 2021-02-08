3ba8368bbb80ded131e84d513fdab1f045813878
Knight <mayanks365@gmail.com>
2021-02-08 11:06:02 +0530

    updating files

---
+++ Flex/EdgeLedger/trail.py
@@ -0,0 +1,10 @@
+import subprocess
+import os
+
+# p1 = subprocess.run(['mkdir', '/Users/knight/Desktop/backup'])
+# p1.stdout
+
+# it will create a folder named backup
+# subprocess.Popen(['cd', '/Users/knight/Desktop'])
+
+os.system("mkdir /Users/knight/Desktop/made")
